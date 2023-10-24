4a73d0a7aa79796cbe617676fd750f93e951b196
nguyentinh0310 <nguyenquoctinh0310@gmail.com>
2023-10-24 23:30:30 +0700

    set up mongodb driver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { Database } from './config/mongodb';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Database],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
+import helmet from 'helmet';
+import * as morgan from 'morgan';
 import { AppModule } from './app.module';
+import { env } from './config/environment';
+import { Database } from './config/mongodb';
+import * as exitHook from 'async-exit-hook';
 
 async function bootstrap() {
+  const hostname = 'localhost' || env.APP_HOST;
+  const port = env.APP_PORT;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const db = app.get(Database);
+
+  // morgan middleware để ghi log
+  app.use(morgan('dev'));
+  // helmet middleware để bảo mật ứng dụng
+  app.use(helmet());
+  // compression middleware để nén gửi dữ liệu
+  app.use(compression());
+
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is running at http://${hostname}:${port}/`);
+  });
+
+  // Thực hiện tác vụ cleanup trước khi dừng server
+  exitHook(() => {
+    console.log('Disconnection from MongoDB Cloud Atlas');
+    db.onModuleDestroy();
+  });
 }
-bootstrap();
+
+(async () => {
+  try {
+    await bootstrap();
+  } catch (error) {
+    console.log(error);
+    process.exit(0);
+  }
+})();
